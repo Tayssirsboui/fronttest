276f4725922b48db3ef227de1f68060eaabd83ab
tayssirsboui <tayssirsboui@gmail.com>
2025-04-04 00:21:23 -0700

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'revisionproject'`, () => {
+  it(`should have as title 'fronttest'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('revisionproject');
+    expect(app.title).toEqual('fronttest');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('revisionproject app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('fronttest app is running!');
   });
 });
