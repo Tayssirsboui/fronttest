d8ccc9ed2071d3b875a850515c36e0b173674c55
tayssirsboui <tayssirsboui@gmail.com>
2025-04-14 22:00:40 -0700

    FO+BO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './front/footer/footer.component';
+import { HeaderComponent } from './front/header/header.component';
+import { HomeComponent } from './front/home/home.component';
+import { NavbarComponent } from './front/navbar/navbar.component';
+import { AboutComponent } from './front/about/about.component';
+import { CoursesComponent } from './front/courses/courses.component';
+import { FooterBackComponent } from './back/footer-back/footer-back.component';
+import { SidebarBackComponent } from './back/sidebar-back/sidebar-back.component';
+import { BackLayoutComponent } from './back/back-layout/back-layout.component';
+import { FrontLayoutComponent } from './front/front-layout/front-layout.component';
+import { DashboardComponent } from './back/dashboard/dashboard.component';
+import { HeaderBackComponent } from './back/header-back/header-back.component';
+import { SamplePageComponent } from './back/sample-page/sample-page.component';
+import { EventsComponent } from './front/events/events.component';
+import { CourseSingleComponent } from './front/course-single/course-single.component';
+import { TeachersComponent } from './front/teachers/teachers.component';
+import { ContactComponent } from './front/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    NavbarComponent,
+    AboutComponent,
+    CoursesComponent,
+    FooterBackComponent,
+    SidebarBackComponent,
+    BackLayoutComponent,
+    FrontLayoutComponent,
+    DashboardComponent,
+    HeaderBackComponent,
+    SamplePageComponent,
+    EventsComponent,
+    CourseSingleComponent,
+    TeachersComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
