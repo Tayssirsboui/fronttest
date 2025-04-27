1032dcaaa75b6ec98ae0ee8a9c104b56b81fef92
Alasiuidi <yayay9676@gmail.com>
2025-04-27 03:51:59 +0100

    user+quiz+blogs+event+projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -78,11 +78,21 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { AdminGestionEvenementsComponent } from './back/admin-gestion-evenements/admin-gestion-evenements.component';
 import { StatsEvenementModalComponent } from './back/stats-evenement-modal/stats-evenement-modal.component';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { ListeParticipationsComponent } from './back/liste-participations/liste-participations.component';
+import { ListProjetsComponent } from './back/Gestion-Projets/list-projets/list-projets.component';
+import { AjouterCollaborationComponent } from './front/Gestion-Projets/ajouter-collaboration/ajouter-collaboration.component';
+import { AjouterProjetComponent } from './front/Gestion-Projets/ajouter-projet/ajouter-projet.component';
+import { CollaborationsComponent } from './front/Gestion-Projets/collaborations/collaborations.component';
+import { DashboardKanbanComponent } from './front/Gestion-Projets/dashboard-kanban/dashboard-kanban.component';
+import { ProjetsComponent } from './front/Gestion-Projets/projets/projets.component';
+import { RoadmapModalComponent } from './front/Gestion-Projets/roadmap-modal/roadmap-modal.component';
+import { FilterPipe } from './front/Gestion-Projets/shared/filter.pipe';
+import { KanbanModule } from '@syncfusion/ej2-angular-kanban';
 
 
 
@@ -115,7 +125,6 @@ import { ListeParticipationsComponent } from './back/liste-participations/liste-
     BstagesComponent,
     BlogComponent,
     BlogDetailsComponent,
-    FooterBackComponent,
     GeminiChatComponent,
     BlogAdminComponent,
     MesPostsComponent,
@@ -138,71 +147,61 @@ import { ListeParticipationsComponent } from './back/liste-participations/liste-
     AdminGestionEvenementsComponent,
     StatsEvenementModalComponent,
     ListeParticipationsComponent,
+    ProjetsComponent,
+    FilterPipe,
+    AjouterCollaborationComponent,
+    AjouterProjetComponent,
+    CollaborationsComponent,
+    DashboardKanbanComponent,
+    RoadmapModalComponent,
+    ListProjetsComponent
   
   ],
  
   imports: [
-    HttpClientModule,
-    BrowserModule,
-    
-    NgChartsModule,
-    ReactiveFormsModule, 
-    
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    WebcamModule,
-    CodeInputModule,
-    NgbModule,
-    CommonModule,
-    PickerModule,
-    BrowserAnimationsModule,
-    ToastrModule.forRoot({
-      progressBar:true,
-      closeButton:true,
-      newestOnTop:true,
-      tapToDismiss:true,
-      positionClass:'toast-top-right',
-      timeOut: 8000,
-
-    }),
-   
-    
-    RouterModule,
-    FullCalendarModule, 
-    FormsModule,
-    MatDialogModule, 
-    MatFormFieldModule, 
-    MatInputModule,
-    MatButtonModule,
-    NgbModule,
-    NgbModalModule,
-    MatSelectModule,
-    BrowserAnimationsModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    NgxMatDatetimePickerModule,
-    NgxMatTimepickerModule,
-    NgxMatMomentModule,
-    NgxMatNativeDateModule,
-    MatDialogModule,
-    MatProgressBarModule,
-    MatDividerModule,
-    MatCardModule,
-    MatButtonModule,
-    MatIconModule,
-    MatProgressSpinnerModule,
-    GoogleMapsModule,
-    MatSnackBarModule,
-  
-    MatDialogModule,
-    ToastrModule.forRoot({
-      positionClass: 'toast-top-right',
-      timeOut: 3000,
-      closeButton: true,
-      progressBar: true,
-      preventDuplicates: true
-    }),
+  HttpClientModule,
+  BrowserModule,
+  NgChartsModule,
+  ReactiveFormsModule,
+  AppRoutingModule,
+  FormsModule,
+  WebcamModule,
+  CodeInputModule,
+  NgbModule,
+  CommonModule,
+  PickerModule,
+  BrowserAnimationsModule,
+  ToastrModule.forRoot({
+    progressBar: true,
+    closeButton: true,
+    newestOnTop: true,
+    tapToDismiss: true,
+    positionClass: 'toast-top-right',
+    timeOut: 8000,
+  }),
+  RouterModule,
+  FullCalendarModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatButtonModule,
+  NgbModalModule,
+  MatSelectModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  NgxMatDatetimePickerModule,
+  NgxMatTimepickerModule,
+  NgxMatMomentModule,
+  NgxMatNativeDateModule,
+  MatProgressBarModule,
+  MatDividerModule,
+  MatCardModule,
+  MatIconModule,
+  MatProgressSpinnerModule,
+  GoogleMapsModule,
+  MatSnackBarModule,
+  MatPaginatorModule,
+  KanbanModule
   ],
   
   providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy },
@@ -213,104 +212,5 @@ import { ListeParticipationsComponent } from './back/liste-participations/liste-
   bootstrap: [AppComponent]
 })
 
-export class AppModule { }import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FooterComponent } from './front/footer/footer.component';
-import { HeaderComponent } from './front/header/header.component';
-import { HomeComponent } from './front/home/home.component';
-import { NavbarComponent } from './front/navbar/navbar.component';
-import { AboutComponent } from './front/about/about.component';
-import { CoursesComponent } from './front/courses/courses.component';
-import { FooterBackComponent } from './back/footer-back/footer-back.component';
-import { SidebarBackComponent } from './back/sidebar-back/sidebar-back.component';
-import { BackLayoutComponent } from './back/back-layout/back-layout.component';
-import { FrontLayoutComponent } from './front/front-layout/front-layout.component';
-import { DashboardComponent } from './back/dashboard/dashboard.component';
-import { HeaderBackComponent } from './back/header-back/header-back.component';
-import { SamplePageComponent } from './back/sample-page/sample-page.component';
-import { EventsComponent } from './front/events/events.component';
-import { CourseSingleComponent } from './front/course-single/course-single.component';
-import { TeachersComponent } from './front/teachers/teachers.component';
-import { ContactComponent } from './front/contact/contact.component';
-import { ProjetsComponent } from './front/Gestion-Projets/projets/projets.component';
-import { AjouterCollaborationComponent } from './front/Gestion-Projets/ajouter-collaboration/ajouter-collaboration.component';
-import { AjouterProjetComponent } from './front/Gestion-Projets/ajouter-projet/ajouter-projet.component';
-import { CollaborationsComponent } from './front/Gestion-Projets/collaborations/collaborations.component';
-import { FilterPipe } from './front/Gestion-Projets/shared/filter.pipe';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-import { MatDialogModule } from '@angular/material/dialog';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material/form-field'; 
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { LocationStrategy, HashLocationStrategy, PathLocationStrategy } from '@angular/common';
-import {  MatPaginatorModule } from '@angular/material/paginator';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { KanbanModule } from '@syncfusion/ej2-angular-kanban';
-import { DashboardKanbanComponent } from './front/Gestion-Projets/dashboard-kanban/dashboard-kanban.component';
-import { RoadmapModalComponent } from './front/Gestion-Projets/roadmap-modal/roadmap-modal.component';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatIconModule } from '@angular/material/icon';
-import { ListProjetsComponent } from './back/Gestion-Projets/list-projets/list-projets.component';
-@NgModule({
-  declarations: [
-    AppComponent,
-    FooterComponent,
-    HeaderComponent,
-    HomeComponent,
-    NavbarComponent,
-    AboutComponent,
-    CoursesComponent,
-    FooterBackComponent,
-    SidebarBackComponent,
-    BackLayoutComponent,
-    FrontLayoutComponent,
-    DashboardComponent,
-    HeaderBackComponent,
-    SamplePageComponent,
-    EventsComponent,
-    CourseSingleComponent,
-    TeachersComponent,
-    ContactComponent,
-    ProjetsComponent,
-    FilterPipe,
-    AjouterCollaborationComponent,
-    AjouterProjetComponent,
-    CollaborationsComponent,
-    DashboardKanbanComponent,
-    RoadmapModalComponent,
-    ListProjetsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    FormsModule, 
-    ReactiveFormsModule,
-    NgbModule,
-    MatDialogModule, 
-    MatFormFieldModule, 
-    MatInputModule,
-    MatButtonModule,
-    MatSelectModule,
-    MatPaginatorModule,
-    MatSnackBarModule,
-    KanbanModule,
-    MatProgressSpinnerModule,
-    MatProgressBarModule,
-    MatIconModule
-
-
-  ],
-  providers: [{ provide: LocationStrategy, useClass: PathLocationStrategy }],
-  bootstrap: [AppComponent]
-})
 export class AppModule { }
